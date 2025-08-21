d9c559ca50a71456f9331812210e8b3c65a4677e
Emteyaj-Ahamad7827 <emteyajahmad10@gmail.com>
2025-08-21 11:26:09 +0530

    switch case loop

---
+++ js/loops.js
@@ -0,0 +1,30 @@
+// for loop
+
+const arr = [1,2,3,4,5,6,7,8,9,10];
+console.log("arr",arr.length; i++) {
+    for (let i = 0; i < arr.length; i++)
+}
+
+
+/* while loop */
+let num = 9;
+let i = 0; // 
+let sum = null;
+while (i < num) {
+    sum += i;
+    i++;
+
+}
+console.log(~ sum:)
+
+
+
+
+
+// do while loop //
+let number = 6;
+
+do {
+    console.log(number); 6, 7, 8, 9
+    number++;
+} while (numbers < 10);
