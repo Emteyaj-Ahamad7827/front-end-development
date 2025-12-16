74f85856810b2144b603eb185bbd8c16bbdb7c59
Emteyaj-Ahamad7827 <emteyajahmad10@gmail.com>
2025-12-16 11:14:21 +0530

    import and export

---
+++ js/import&export/export.js
@@ -0,0 +1,25 @@
+const sumOfTwoNumber = (a, b) => {
+  return a + b;
+};
+
+const subOfTwoNumber = (a, b) => {
+  return a - b;
+};
+
+export { sumOfTwoNumber, subOfTwoNumber };
+
+// name export
+
+//=============================================//
+
+// const sumOfTwoNumber = (a, b) => {
+//   return a + b;
+// };
+
+// const subOfTwoNumber = (a, b) => {
+//   return a - b;
+// };
+
+// export default { sumOfTwoNumber, subOfTwoNumber };
+
+// default export
