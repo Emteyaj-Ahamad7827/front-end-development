8ea4e8f1c30c21d00579891319983046c91d7fe9
Emteyaj-Ahamad7827 <emteyajahmad10@gmail.com>
2025-12-21 11:14:51 +0530

    code of the day

--- js/ls/js.js/index.js
+++ js/ls/js.js/index.js
@@ -1,42 +1,94 @@
+// const API = 'https://fakestoreapi.com/products';
+
+// async function ApiCall() {
+//     // fetch(API)
+//     // .then((res) => res.json())
+//     // .then((res) => dataAppend(res))
+//     // .catch((err) => console.log(err))
+
+//     try {
+//         const res = await fetch(API);
+//         const data = await res.json();
+//         dataAppend(data)
+//     } catch (error) {
+//         console.log('error', error);
+        
+//     }
+// }
+// ApiCall();
+
+// function dataAppend(value) {
+//     console.log('value', value);
+
+//     const mainDiv = document.querySelector('#mainData');
+
+//     value?.forEach(el => {
+//         const childDiv = document.createElement('div');
+//         const id = document.createElement('h3');
+//         const img = document.createElement('img');
+//         const title = document.createElement('h3');
+//         const description = document.createElement('h3');
+//         const price = document.createElement('h3');
+//         const category = document.createElement('h3');
+
+//         id.innerText = el.id;
+
+//         img.src = el.imgage;
+//         title.innerText = el.title;
+//         description.innerText = el.description;
+//         price.innerText = el.price;
+//         category.innerText = el.category;
+//     });
+    
+// }
+
+
+// new code
+
 const API = 'https://fakestoreapi.com/products';
 
 async function ApiCall() {
-    // fetch(API)
-    // .then((res) => res.json())
-    // .then((res) => dataAppend(res))
-    // .catch((err) => console.log(err))
-
     try {
         const res = await fetch(API);
         const data = await res.json();
+        dataAppend(data);
     } catch (error) {
         console.log('error', error);
-        
     }
 }
+
 ApiCall();
 
 function dataAppend(value) {
-    console.log('value', value);
-
     const mainDiv = document.querySelector('#mainData');
 
-    value?.forEach(el => {
-        const childDiv = document.getElement('div');
-        const id = document.getElement('h3');
-        const img = document.getElement('img');
-        const title = document.getElement('h3');
-        const description = document.getElement('h3');
-        const price = document.getElement('h3');
-        const category = document.getElement('h3');
+    value.forEach(el => {
+        const childDiv = document.createElement('div');
 
-        id.innerText = el.id;
+        const id = document.createElement('h3');
+        const img = document.createElement('img');
+        const title = document.createElement('h3');
+        const description = document.createElement('p');
+        const price = document.createElement('h3');
+        const category = document.createElement('h3');
 
-        img.innerText = el.img;
+        id.innerText = `ID: ${el.id}`;
+        img.src = el.image;
+        img.style.width = '150px';
         title.innerText = el.title;
         description.innerText = el.description;
-        price.innerText = el.price;
+        price.innerText = `Price: $${el.price}`;
         category.innerText = el.category;
+
+        childDiv.append(
+            id,
+            img,
+            title,
+            description,
+            price,
+            category
+        );
+
+        mainDiv.appendChild(childDiv);
     });
-    
 }
--- js/ls/js.js/sign_up.js
+++ js/ls/js.js/sign_up.js
@@ -1,30 +1,57 @@
-const storeData = JSON.parse(localStorage.getItem('userData')) || [];
+// const storeData = JSON.parse(localStorage.getItem('userData')) || [];
 
-//nlet location = window.location.pathname;
-// console.log('location', location);
+// // let location = window.location.pathname;
+// // console.log('location', location);
+
+// const signUp = () => {
+//     let userName = document.getElementById('signUsername').value.trim();
+//     console.log('userName', userName);
+    
+//     let age = document.getElementById('SignAge').value.trim();
+//     console.log('age', age);
+//     let password = document.getElementById('signPassword').value.trim();
+//     console.log('password', password);
+    
+//     let singlePersonData = {
+//         user: userName,
+//         age,
+//         pass: password
+//     };
+//     storeData.push(singlePersonData);
+
+//     localStorage.setItem('userData', JSON.stringify(storeData));
+
+//     setTimeout(() => {
+//         alert('data save successfully');
+//         window.location = '.ls/pages/Login.html';
+
+//     }, 100);
+    
+// };
+
+
+// new code
+
+const storeData = JSON.parse(localStorage.getItem('userData')) || [];
 
 const signUp = () => {
     let userName = document.getElementById('signUsername').value.trim();
-    console.log('userName', userName);
-    
     let age = document.getElementById('SignAge').value.trim();
-    console.log('age', age);
     let password = document.getElementById('signPassword').value.trim();
-    console.log('password', password);
-    
-    let singlePersonData = {
+
+    if (!userName || !age || !password) {
+        alert('Please fill all fields');
+        return;
+    }
+
+    storeData.push({
         user: userName,
         age,
         pass: password
-    };
-    storeData.push(singlePersonData);
+    });
 
     localStorage.setItem('userData', JSON.stringify(storeData));
 
-    setTimeout(() => {
-        alert('data save successfully');
-        window.location = '.ls/pages/Login.html';
-
-    }, 100);
-    
+    alert('Data saved successfully');
+    window.location.href = './pages/Login.html';
 };
